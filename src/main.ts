190b712385e31dd45fe149077e131dece76843ca
a.kashina <a.kashina@websecret.by>
2023-07-29 19:03:13 +0300

    feat: (/doc) read docs from /doc/api.yaml

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import * as yaml from 'js-yaml';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
 const PORT = process.env.PORT || 4000;
 
@@ -12,6 +16,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const document = yaml.load(
+    (await readFile(join(process.cwd(), 'doc/api.yaml'))).toString('utf-8'),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();
