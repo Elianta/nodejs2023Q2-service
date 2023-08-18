c2481a109edf86df8a27d6a3b5f6be1846d00d84
a.kashina <a.kashina@websecret.by>
2023-08-18 19:42:16 +0200

    feat: write logs to a file

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,58 @@
-import { Injectable, Scope, ConsoleLogger } from '@nestjs/common';
+import { Injectable, Scope, ConsoleLogger, LogLevel } from '@nestjs/common';
+import { writeFile } from 'fs/promises';
 
 @Injectable({ scope: Scope.TRANSIENT })
-export class LoggerService extends ConsoleLogger {}
+export class LoggerService extends ConsoleLogger {
+  private static LOG_FILE_PATH = 'logs/log.txt';
+
+  error(message: string, stackOrContext?: string) {
+    super.error(message, stackOrContext);
+
+    const formattedMessage = this.formatLogMessage(
+      'error',
+      message,
+      stackOrContext,
+    );
+    this.writeToFile(formattedMessage);
+  }
+
+  warn(message: string, context?: string) {
+    super.warn(message, context);
+
+    const formattedMessage = this.formatLogMessage('warn', message, context);
+    this.writeToFile(formattedMessage);
+  }
+
+  log(message: string, context?: string) {
+    super.log(message, context);
+
+    const formattedMessage = this.formatLogMessage('log', message, context);
+    this.writeToFile(formattedMessage);
+  }
+
+  debug(message: string, context?: string) {
+    super.debug(message, context);
+
+    const formattedMessage = this.formatLogMessage('debug', message, context);
+    this.writeToFile(formattedMessage);
+  }
+
+  verbose(message: string, context?: string) {
+    super.verbose(message, context);
+
+    const formattedMessage = this.formatLogMessage('verbose', message, context);
+    this.writeToFile(formattedMessage);
+  }
+
+  private writeToFile(formattedMessage: string) {
+    writeFile(LoggerService.LOG_FILE_PATH, formattedMessage, { flag: 'a' });
+  }
+
+  private formatLogMessage(
+    logLevel: LogLevel,
+    message: string,
+    context: string = '',
+  ) {
+    return `${this.getTimestamp()}\t${logLevel.toUpperCase()}\t[${context}] ${message}\n`;
+  }
+}
